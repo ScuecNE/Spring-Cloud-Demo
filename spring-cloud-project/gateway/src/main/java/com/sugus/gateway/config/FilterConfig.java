0eeb3e40f97d40246562cc282cdcf7d4167fcaf6
suguswong <suguswong@outlook.com>
2019-07-15 15:36:06 +0800

    gateway新增熔断机制，令牌桶算法限流上升到全局filter

--- spring-cloud-project/gateway/src/main/java/com/sugus/gateway/config/FilterConfig.java
+++ spring-cloud-project/gateway/src/main/java/com/sugus/gateway/config/FilterConfig.java
@@ -1,8 +1,11 @@
 package com.sugus.gateway.config;
 
+import com.sugus.gateway.filter.RateLimitByIpGatewayFilter;
 import com.sugus.gateway.filter.TokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 @Configuration
 public class FilterConfig {
@@ -10,6 +13,11 @@ public class FilterConfig {
     @Bean
     public TokenFilter tokenFilter(){
         return new TokenFilter();
+    }
+
+    @Bean
+    public RateLimitByIpGatewayFilter rateLimitByIpGatewayFilter(){
+        return new RateLimitByIpGatewayFilter(10, 1, Duration.ofSeconds(1));
     }
 
 }
